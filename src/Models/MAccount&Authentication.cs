c3b6eb96c040f47d9e799899ff583ea6e4cca3eb
hvu578376-dotcom <hvu578376@gmail.com>
2026-08-23 22:33:54 +0700

    first commit

---
+++ src/Models/MAccount&Authentication.cs
@@ -0,0 +1,8 @@
+public class MAccount
+{
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public string? Role { get; set; }
+}
